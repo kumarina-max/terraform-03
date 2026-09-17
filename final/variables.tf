01e4d9f04c4009157ea5e03f845d6918e2c53115
Marina Kukushkina <kukushkina_8704@mail.ru>
2026-09-17 13:55:19 +0300

    Add final project Terraform skeleton

---
+++ final/variables.tf
@@ -0,0 +1,37 @@
+variable "service_account_key_file" {
+  type        = string
+  description = "Path to service account authorized key"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "Yandex Cloud ID"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Yandex Cloud Folder ID"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "Default availability zone"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "final-project"
+  description = "VPC network name"
+}
+
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.10.0.0/24"]
+  description = "Subnet CIDR"
+}
+
+variable "vms_ssh_root_key" {
+  type        = string
+  description = "SSH public key"
+}
